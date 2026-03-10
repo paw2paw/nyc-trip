5c33beb206c7687c723026cf96b36c1c9aa46e86
paw2paw <gh@thewanders.com>
2026-03-10 16:34:47 +0000

    reset app to stable baseline

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let data
 let dayIndex=0
-let stopIndex=0
 
 fetch("data.json")
 .then(r=>r.json())
@@ -10,54 +9,22 @@ render()
 })
 
 function render(){
-
 let day=data.days[dayIndex]
-
 document.getElementById("dayMeta").innerText="DAY "+(dayIndex+1)
 document.getElementById("title").innerText=day.title
-
-let stopsHTML=""
-
+let html=""
 day.stops.forEach((s,i)=>{
-stopsHTML+=`
-<div class="stop" onclick="setStop(${i})">
-<b>${i+1}. ${s.name}</b><br>
-${s.address}
-</div>`
+html+=`<div class="stop">${i+1}. ${s.name}<br>${s.address}</div>`
 })
-
-document.getElementById("stops").innerHTML=stopsHTML
-
-}
-
-function setStop(i){
-stopIndex=i
+document.getElementById("stops").innerHTML=html
 }
 
 function toggleMenu(){
-let m=document.getElementById("menu")
-let o=document.getElementById("menuOverlay")
-
-m.classList.toggle("open")
-o.classList.toggle("show")
+document.getElementById("menu").classList.toggle("open")
 }
 
-function returnHotel(){
-alert("Return to hotel")
-}
-
-function showMap(){
-alert("Map view")
-}
-
-function nearbyCoffee(){
-window.open("https://maps.google.com/search/coffee+near+me")
-}
-
-function nearbyFood(){
-window.open("https://maps.google.com/search/food+near+me")
-}
-
-function sendMyLocation(){
-alert("Send location")
-}
+function returnHotel(){alert("Return Hotel")}
+function showMap(){alert("Map")}
+function nearbyCoffee(){window.open("https://maps.google.com/search/coffee+near+me")}
+function nearbyFood(){window.open("https://maps.google.com/search/food+near+me")}
+function sendMyLocation(){alert("Location")}
